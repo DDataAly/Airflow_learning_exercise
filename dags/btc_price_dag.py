21083fe03ebf88d4df2af8ab9cd1505d8264f9d3
DDataAly <alyona.dorof@gmail.com>
2025-10-02 16:59:45 +0100

    Changing the extract logic to imitate nested loop logic

--- dags/btc_price_dag.py
+++ dags/btc_price_dag.py
@@ -17,24 +17,65 @@ def get_best_price_for_required_order_volume():
     order_volume = float(Variable.get("order_volume", default=0.5))
     num_trans = int(Variable.get("num_trans", default=3))
     vol_per_order = order_volume / num_trans
-    
-    @task()
-    def get_individual_quotes():    
-        """
-        Make individual API calls to 3 exchanges; repeat num_trans times
-        """
-        exchanges = set_up_exchanges()
+
+    exchanges = set_up_exchanges()
         
-        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        path = os.path.join(base_dir, "data")
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    path = os.path.join(base_dir, "data")
 
-        for i in range(0, num_trans):
-            print("Fetching the prices across exchanges...")
-            request_time = get_request_time()
-            print(request_time)
+    @task
+    def api_calls_per_exchange(num_trans):
+        return list(range(num_trans))  #[0, 1, 2]
 
-            get_price (exchanges, path, request_time)
+    # @task
+    # def get_request_time():
+    #     print("Fetching the prices across exchanges...")
+    #     request_time = get_request_time()
+    #     print(request_time)
+    #     return(request_time)
 
-    get_individual_quotes()
+    @task
+    def prepare_exchange_calls(exchanges, num_trans):
+        return [(exchange, i) for i in range(num_trans) for exchange in exchanges]
+    
+    @task
+    def make_api_call(exchange, path, request_time):
+        return get_price (exchange, path, request_time)
+    
+    iterations = api_calls_per_exchange(num_trans = num_trans) #using task(param=value) Airflow syntax 
+    # request_time = get_request_time()
+    make_api_call.expand(exchange = exchanges, iteration = iterations) #mapping over multiple parameters -  nested loop logic but order of execution is not guaranteed; only keyword args are allowed
 
-dag_instance = get_best_price_for_required_order_volume()    
+
+
+
+
+
+
+
+
+
+
+    
+#     @task()
+#     def get_individual_quotes():    
+#         """
+#         Make individual API calls to 3 exchanges; repeat num_trans times
+#         """
+#         exchanges = set_up_exchanges()
+        
+#         base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#         path = os.path.join(base_dir, "data")
+
+#         for i in range(0, num_trans):
+#             print("Fetching the prices across exchanges...")
+#             request_time = get_request_time()
+#             print(request_time)
+
+#             for exchange in exchanges:
+#                 get_price (exchange, path, request_time)
+
+#     get_individual_quotes()
+
+# dag_instance = get_best_price_for_required_order_volume()    
+
