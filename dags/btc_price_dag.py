8add9e95d0da8957038ad4d16fcd895728fd7783
DDataAly <alyona.dorof@gmail.com>
2025-10-03 11:41:40 +0100

    Creates separate dag for the extract stage only

--- dags/btc_price_dag.py
+++ dags/btc_price_dag.py
@@ -4,53 +4,98 @@ from src.utils.helpers import set_up_exchanges, get_request_time
 from src.utils.price_extract import get_price
 from airflow.sdk import Variable, dag, task
 
+
+def compute_schedule(trans_time: int, num_trans: int):
+    interval_minutes = max(1, (trans_time // num_trans) // 60)
+    return pendulum.duration(minutes=interval_minutes)
+
 @dag(
-    schedule=pendulum.duration(days=1),
+    schedule=compute_schedule(
+        int(Variable.get("trans_time", default=180)), #time in sec
+        int(Variable.get("num_trans", default=3))
+    ),
     start_date=pendulum.datetime(2025, 9, 30, 11, 30, tz="Europe/London"),
     catchup=False,
     tags=["personal projects"],
 )
-def get_best_price_for_required_order_volume():
-    """
-    Returns best price for the total order volume
-    """
-    order_volume = float(Variable.get("order_volume", default=0.5))
-    num_trans = int(Variable.get("num_trans", default=3))
-    vol_per_order = order_volume / num_trans
 
+def get_order_book_snapshots():
+    
     exchanges = set_up_exchanges()
-        
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     path = os.path.join(base_dir, "data")
 
     @task
-    def api_calls_per_exchange(num_trans):
-        return list(range(num_trans))  #[0, 1, 2]
+    def fetch_snapshot(exchange):
+        request_time = get_request_time()
+        get_price(exchange, path, request_time)
+        return f"{exchange.name} snapshot at {request_time}"
 
-    # @task
-    # def get_request_time():
-    #     print("Fetching the prices across exchanges...")
-    #     request_time = get_request_time()
-    #     print(request_time)
-    #     return(request_time)
+    fetch_snapshot.expand(exchange=exchanges)
 
-    @task
-    def prepare_exchange_calls(exchanges, num_trans):
-        return [(exchange, i) for i in range(num_trans) for exchange in exchanges]
-    
-    @task
-    def make_api_call(exchange, path, request_time):
-        return get_price (exchange, path, request_time)
-    
-    iterations = api_calls_per_exchange(num_trans = num_trans) #using task(param=value) Airflow syntax 
-    # request_time = get_request_time()
-    make_api_call.expand(exchange = exchanges, iteration = iterations) #mapping over multiple parameters -  nested loop logic but order of execution is not guaranteed; only keyword args are allowed
+dag_instance = get_order_book_snapshots()
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+# import os
+# import pendulum
+# from src.utils.helpers import set_up_exchanges, get_request_time
+# from src.utils.price_extract import get_price
+# from airflow.sdk import Variable, dag, task
+
+# @dag(
+#     schedule=pendulum.duration(days=1),
+#     start_date=pendulum.datetime(2025, 9, 30, 11, 30, tz="Europe/London"),
+#     catchup=False,
+#     tags=["personal projects"],
+# )
+# def get_best_price_for_required_order_volume():
+#     """
+#     Returns best price for the total order volume
+#     """
+#     order_volume = float(Variable.get("order_volume", default=0.5))
+#     num_trans = int(Variable.get("num_trans", default=3))
+#     vol_per_order = order_volume / num_trans
+
+#     exchanges = set_up_exchanges()
+        
+#     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#     path = os.path.join(base_dir, "data")
+ 
+#     @task
+#     def prepare_exchange_calls(exchanges, num_trans):
+#         return [(exchange, i) for i in range(num_trans) for exchange in exchanges]
+
+#     @task
+#     def get_request_time_task():
+#         rt = get_request_time()
+#         return rt
+
+#     @task
+#     def make_api_call(params, path, request_time):
+#         exchange, iteration = params
+#         return get_price(exchange, path, request_time)
+
+#     calls = prepare_exchange_calls(exchanges, num_trans)
+#     request_time = get_request_time_task()
+#     make_api_call.expand(params=calls, path=path, request_time=request_time)
+
+# dag_instance = get_best_price_for_required_order_volume()
 
 
 
