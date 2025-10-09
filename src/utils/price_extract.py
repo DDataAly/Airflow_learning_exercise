082806b47b7758896ab8d3d7bb53f87f24fdbde3
DDataAly <alyona.dorof@gmail.com>
2025-10-09 15:03:50 +0100

    Removes save-to-file logic as we use Xcom for it

--- src/utils/price_extract.py
+++ src/utils/price_extract.py
@@ -1,38 +1,21 @@
 import logging
 from datetime import datetime
 
-def get_price(exchange, path: str, request_time: datetime ) -> list:
+def get_price(exchange, request_time: datetime ) -> str:
     try:
-        snapshot = exchange.request_order_book_snapshot()
-        # if snapshot is None or exchange.validate_snapshot(snapshot) is False:
-        if exchange.validate_snapshot(snapshot) is False:
-            logging.warning(
-                f"{exchange.name}: invalid or missing snapshot at {request_time}"
-            )
-            # continue
+            snapshot = exchange.request_order_book_snapshot()
+            if not exchange.validate_snapshot(snapshot):
+                logging.warning(f"{exchange.name}: invalid snapshot at {request_time}")
+                return None
 
-        snapshot = exchange.trim_snapshot(snapshot)
-        exchange.save_order_book_snapshot(snapshot, path, request_time)
-
+            trimmed = exchange.trim_snapshot(snapshot)
+            return {
+                "exchange": exchange.name,
+                "request_time": request_time,
+                "snapshot": trimmed,
+            }
+    
     except Exception as e:
         logging.error(f"{exchange.name} An error has occurred at the extract stage: {e}")
-
-
-
-# def get_price(exchanges: list, path: str, request_time: datetime ) -> list:
-#     for exchange in exchanges:
-#         try:
-#             snapshot = exchange.request_order_book_snapshot()
-#             # if snapshot is None or exchange.validate_snapshot(snapshot) is False:
-#             if exchange.validate_snapshot(snapshot) is False:
-#                 logging.warning(
-#                     f"{exchange.name}: invalid or missing snapshot at {request_time}"
-#                 )
-#                 continue
-
-#             snapshot = exchange.trim_snapshot(snapshot)
-#             exchange.save_order_book_snapshot(snapshot, path, request_time)
-
-#         except Exception as e:
-#             logging.error(f"{exchange.name} An error has occurred at the extract stage: {e}")
-
+        return None
+    
